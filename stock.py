ffaafd7a9a3964007c546bc8a62e4d2744297b59
Mohammad Shams <gr8linux@yahoo.com>
2020-01-25 21:53:57 +0330

    add rahavard & tseclient reader

--- stock.py
+++ stock.py
@@ -1,5 +1,7 @@
 from tehran_stocks import Stocks,db,get_asset
 import pandas as pd
+import matplotlib.pyplot as plt
+import mplfinance as mpf
 #pd.set_option('plotting.backend', 'pandas_bokeh')
 import matplotlib.pyplot as plt
 '''tseStock handle basic operations on TSE data fetch from 
@@ -9,7 +11,11 @@ import matplotlib.pyplot as plt
     mystock.plot()              # Plot the close price column with pandas.plot function
 '''
 #TODO Import adjusted data form Tehran stock exchange
+#TODO Developed based on mplfinance and add your extra feather
+#TODO Build a better test module
+
 class tseStock():
+    ASSET_PATH = "/home/ali/Documents/books/tse/d/"
     def __init__(self,name,start_date=None,end_date=None):
         self.name = name
         self.__stock__ = get_asset(self.name)
@@ -33,7 +39,6 @@ class tseStock():
                 self.__price__['MA' + str(window)] = self.__price__.close.rolling(window).mean()
                 return self.__price__['MA'+str(window)]
             else:
-                #self.__price__['MA' + str(window)] = self.__price__.close.rolling(window).mean()
                 return self.__price__['MA' + str(window)]
         else:
            return None
@@ -52,8 +57,33 @@ class tseStock():
     @close.setter
     def close(self,close):
         self.__price__.close=close
+    @property
+    def mpl(self):
+        return self.__stock__.mpl
+    @staticmethod
+    def asset_load(ticker=None, start=None, end=None, fullpath=None,source='rahavard'):
+        # loading the data from rahavad365 .txt data file
+        # rahavad generating a CSV format of the assets in daily time frame
+        # ASSET_PATH is the root directory of asset files
+        if(ticker==None and fullpath==None):
+            return None
+        if (fullpath == None):
+            path = tseStock.ASSET_PATH + ticker + '_D_SH.txt'
+        else:
+            path = fullpath
+        if(source == 'rahavard'):
+            names = ["Ticker", "Per", "DATE", "TIME", "Open", "High", "Low", "Close", "Volume", "Openint","type", "id"]
+        elif(source == 'tseclient'):
+            names = ["Ticker","DATE","Open","High", "Low", "Close", "Volume", "Value", "Openint", "TradeCount", "Code", "Name_en", "Name", "Jdate", "Last_price"]
 
-
+        asset = pd.read_csv(path, \
+                            names=names, \
+                            skiprows=1, usecols=["DATE", "Open", "High", "Low", "Close", "Volume", "Openint"], \
+                            parse_dates=["DATE"], index_col=['DATE'])
+        # asset_returns(asset)
+        if (start != None and end != None):
+            asset = asset[start:end]
+        return asset
 
     def __repr__(self):
         return self.name
@@ -64,22 +94,29 @@ def stfinder():
     for i in Stocks.query.filter(Stocks.estimatedEps!=None).all():
         if(len(i.df.close)>0):
             if(i.estimatedEps/i.df.close.iloc[-1] > 0.2 ):
-                print(i.name,i.estimatedEps/i.df.close.iloc[-1])
+                print(f"{i.name} {i.estimatedEps/i.df.close.iloc[-1]:.2f}")
 #stfinder()
-afra = tseStock('فولاد','2017','2020')
-
-#afra.ma(10).plot()
-#afra.df.close.plot()
-print(afra)
-print(afra.close.head())
-#afra.plot('2018','2020')
-
-afra.close.plot(figsize=afra.figsize,legend=True)
-afra.ma(20).plot(figsize=afra.figsize,legend=True)
-
-afra.ma(50).plot(figsize=afra.figsize,legend=True)
-afra.ma(200).plot(figsize=afra.figsize,legend=True,title="Afranet daily close with 3 SMA")
+df = None
+steel = tseStock('فولاد','2017','2020')
+df = tseStock.asset_load('FEOLAD')
+df2 = tseStock.asset_load(fullpath='/home/ali/Documents/books/tse/d/Adjusted/افرا-ت.csv',source='tseclient')
+df.Close.loc['2020'].plot()
+df2.Close.loc['2020'].plot()
 
 plt.show()
+#mpf.plot(steel.mpl)
+#afra.ma(10).plot()
+#afra.df.close.plot()
+#print(afra.__price__.info())
+#print(afra.close.head())
+#afra.plot('2018','2020')
+
+#afra.close.plot(figsize=afra.figsize,legend=True)
+#afra.ma(20).plot(figsize=afra.figsize,legend=True)
+
+#afra.ma(50).plot(figsize=afra.figsize,legend=True)
+#afra.ma(200).plot(figsize=afra.figsize,legend=True,title="Afranet daily close with 3 SMA")
+#mpf.plot(afra.__price__)
+#plt.show()
 
 
